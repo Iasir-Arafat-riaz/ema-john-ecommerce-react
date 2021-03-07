8f6a34f5a4930aecc7d1c99dccd1043eec9ace03
Iasir-Arafat-Riaz <iasir.arafat.riaz@gmail.com>
2021-03-08 03:31:03 +0600

    just commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './components/Header/Header';
-import Shop from './components/shop/shop';
+import Shop from './components/Shop/Shop';
+
 
 
 
@@ -10,6 +11,7 @@ function App() {
     <div>
       <Header></Header>
       <Shop></Shop>
+      
 
     </div>
   );
