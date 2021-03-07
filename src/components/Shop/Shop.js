ce874b1b0fc8e314a93d29096916e8bb504ff6b5
Iasir-Arafat-Riaz <iasir.arafat.riaz@gmail.com>
2021-03-08 03:53:54 +0600

    just commit

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import fake from "../../fakeData"
 
 const Shop = () => {
+    console.log(fake)
+    const firstTen = fake.slice(0,10)
+    const [product,setProduct]=useState(firstTen)
     return (
         <div>
-            hlw
+            
+            <h2>{product.length}</h2>
+            <ul>
+                {product.map(pro=><li>{pro.name}</li>)}
+            </ul>
+            
         </div>
     );
 };
