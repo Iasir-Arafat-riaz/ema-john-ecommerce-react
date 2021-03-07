5b2fd4e84b517ae1d6b98b12d1cd4adec1554315
Iasir-Arafat-Riaz <iasir.arafat.riaz@gmail.com>
2021-03-08 03:55:44 +0600

    just commit

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -3,16 +3,16 @@ import fake from "../../fakeData"
 
 const Shop = () => {
     console.log(fake)
-    const firstTen = fake.slice(0,10)
-    const [product,setProduct]=useState(firstTen)
+    const firstTen = fake.slice(0, 10)
+    const [product, setProduct] = useState(firstTen)
     return (
         <div>
-            
+
             <h2>{product.length}</h2>
             <ul>
-                {product.map(pro=><li>{pro.name}</li>)}
+                {product.map(pro => <li>{pro.name}</li>)}
             </ul>
-            
+
         </div>
     );
 };
