8d614894712f007c741fdd5a9ccf3f41bc64babc
Iasir-Arafat-Riaz <iasir.arafat.riaz@gmail.com>
2021-03-07 09:20:20 +0600

    component commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
+    <div>
+      <h1>hlw</h1>
     </div>
   );
 }
