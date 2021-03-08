81be4469318b1a814e2661786c75879c708a932e
Iasir-Arafat-Riaz <iasir.arafat.riaz@gmail.com>
2021-03-08 06:35:32 +0600

    alignment adjust

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,10 +3,10 @@ import "./Product.css"
 const Product = (props) => {
     return (
         <div className="product">
-            <div>
+            <div className="image">
                 <img src={props.image} alt="" />
             </div>
-            <div>
+            <div className="product-info">
                 <h3>{props.products}</h3>
                 <br />
                 <p>By : {props.seller}</p>
