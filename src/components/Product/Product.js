dc4b747ffca035ac820df67c46792b1f46b9df16
Iasir-Arafat-Riaz <iasir.arafat.riaz@gmail.com>
2021-03-09 23:29:21 +0600

    new commit

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -19,7 +19,7 @@ const Product = (props) => {
                     props.handleButoon(props.price)}
                     className="main-button">
                     <FontAwesomeIcon icon={faShoppingCart} />Add to Cart</button>
-                    <div>12</div>
+                    <div>1245334</div>
             </div>
 
 
