7129e2be5d106c4b787a48f4ddfbfd5b6c7b445f
Iasir-Arafat-Riaz <iasir.arafat.riaz@gmail.com>
2021-04-19 05:36:09 +0600

    new component adding

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,17 +2,18 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import "./Product.css"
+import { Link } from 'react-router-dom';
 
 const Product = (props) => {
     console.log(props.productAll.key)
-    const{img,name,seller,price,stock}=props.productAll
+    const{img,name,seller,price,stock,key}=props.productAll
     return (
         <div className="product">
             <div className="image">
                 <img src={img} alt="" />
             </div>
             <div className="product-info">
-                <h3>{name}</h3>
+                <h3><Link to={"/"+key}>{name}</Link></h3>
                 <br />
                 <p>By : {seller}</p>
                 <p> $ {price}</p>
