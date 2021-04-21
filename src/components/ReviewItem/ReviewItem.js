a92f5424c7685dd8a58dc2e4b290f0e630b81a2e
Iasir-Arafat-Riaz <iasir.arafat.riaz@gmail.com>
2021-04-21 23:42:19 +0600

    new ccommit

--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ReviewItem = (props) => {
     // console.log(props.product.category)
-    const { name, quantity } = props.product;
+    const { name, quantity,img } = props.product;
     const reviewItemStyle = {
         borderBottom: "1px solid lightGray",
         marginBottom: "6px",
@@ -13,6 +13,7 @@ const ReviewItem = (props) => {
     return (
         <div style={reviewItemStyle}>
             <h4>{name}</h4>
+            <img src={img} alt=""/>
             <p>Quantity : {quantity}</p>
             <br />
             <button className="main-button">Remove</button>
